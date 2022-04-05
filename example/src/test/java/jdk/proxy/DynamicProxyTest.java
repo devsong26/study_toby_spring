fc18020162fb8a52e5f7ddf77aa9b5dbabcccfa0
devsong26 <devsong26@gmail.com>
2022-04-05 23:48:17 +0900

    ~ 472

--- example/src/test/java/jdk/proxy/DynamicProxyTest.java
+++ example/src/test/java/jdk/proxy/DynamicProxyTest.java
@@ -5,6 +5,8 @@ import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 
 import java.lang.reflect.Proxy;
 
@@ -63,5 +65,22 @@ public class DynamicProxyTest {
         public String sayThankYou(String name) {
             return "Thank You " + name;
         }
+    }
+
+    @Test
+    public void pointcutAdvisor(){
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget());
+
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut(); // 메서드 이름을 비교해서 대상을 선정하는 알고리즘을 제공하는 포인트컷 생성
+        pointcut.setMappedName("sayH*"); //이름 비교조건 설정. sayH로 시작하는 모든 메서드를 선택하게 한다.
+
+        pfBean.addAdvisor(new DefaultPointcutAdvisor(pointcut, new UppercaseAdvice())); //포인트컷과 어드바이스를 Advisor로 묶어서 한 번에 추가
+
+        Hello proxyHello = (Hello) pfBean.getObject();
+
+        assertEquals(proxyHello.sayHello("Toby"), "HELLO TOBY");
+        assertEquals(proxyHello.sayHi("Toby"), "HI TOBY");
+        assertEquals(proxyHello.sayThankYou("Toby"), "Thank You Toby");
     }
 }
